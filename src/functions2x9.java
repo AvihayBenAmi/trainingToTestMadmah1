171e666781136a048fb83a2bca239adca3c2e842
Avihay Ben Ami <avihaybg3@gmail.com>
2023-01-22 19:47:50 +0200

    22.01.23 11:17

--- src/functions2x9.java
+++ src/functions2x9.java
@@ -1,3 +1,4 @@
+import javax.annotation.processing.SupportedSourceVersion;
 import java.util.Scanner;
 
 public class functions2x9 {
@@ -6,10 +7,10 @@ public class functions2x9 {
         int choice = menu();
         switch (choice) {
             case 1 -> distanceBetweenTowPoints();
-            //case 2 -> lengthOfTheRestTriangle();
-            //case 3 -> circumferenceAndAreaCircle();
-            //case 4 -> squareArea();
-            //case 5 -> rectangleArea();
+            case 2 -> lengthOfTheRestTriangle();
+            case 3 -> perimeterAndAreaCircle();
+            case 4 -> squareArea(choice);
+            case 5 -> squareArea(choice);
         }
     }
 
@@ -20,7 +21,7 @@ public class functions2x9 {
             System.out.println("Welcome to GeoMath Calculator!\nplease enter your choice:\n" +
                     "1. distance between tow points\n" +
                     "2. Calculate the length of the rest in a right triangle\n" +
-                    "3. Calculate the circumference and area of a circle\n" +
+                    "3. Calculate the perimeter and area of a circle\n" +
                     "4. Calculate square area\n" +
                     "5. Calculate rectangle area\n" +
                     "6. Exit");
@@ -48,5 +49,56 @@ public class functions2x9 {
         distance = (Math.pow((x1 - x2), 2)) + (Math.pow((y1 - y2), 2));
         distance = Math.sqrt(distance);
         System.out.println("The distance between (x1,y1),(x2,y2) is " + distance);
+        menu();
+    }
+
+    public static void lengthOfTheRestTriangle() {
+        Scanner scanner = new Scanner(System.in);
+        double a;
+        double b;
+        double c;
+        System.out.println("Enter A rib");
+        a = scanner.nextInt();
+        System.out.println("Enter B rib");
+        b = scanner.nextInt();
+        c = ((Math.pow(a, 2)) + (Math.pow(b, 2)));
+        c = Math.sqrt(c);
+        System.out.println("The rest of Triangle is =" + c);
+        menu();
+    }
+
+    public static void perimeterAndAreaCircle() {
+        Scanner scanner = new Scanner(System.in);
+        int radius;
+        double perimeter;
+        double area;
+        System.out.println("Enter Radius");
+        radius = scanner.nextInt();
+        perimeter = 2 * Math.PI * radius;
+        area = Math.PI * radius * radius;
+        System.out.println("The circle perimeter is =" + perimeter + "\n" +
+                "The circle area is =" + area);
+        menu();
+    }
+
+    public static void squareArea(int choice) {
+        Scanner scanner = new Scanner(System.in);
+        int ribA;
+        int ribB;
+        int area;
+        System.out.println("Enter rib");
+        ribA = scanner.nextInt();
+        if (choice == 4) {
+            area = ribA * ribA;
+            System.out.println("The square area is = " + area);
+            menu();
+        }
+        if (choice == 5) {
+            System.out.println("Enter rib B rectangle");
+            ribB = scanner.nextInt();
+            area = ribA * ribB;
+            System.out.println("The rectangle area is = " + area);
+            menu();
+        }
     }
 }
